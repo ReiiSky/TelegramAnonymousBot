12f6b16278ae916bcf5070443416be2a10dc89a5
Entalizen <danielarya185@gmail.com>
2020-09-14 22:57:31 +0700

    Add: create env system to store api token, and apply it on main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/Satssuki/tele-anon-bot-queue/cmd/server/router"
+	"github.com/Satssuki/tele-anon-bot-queue/pkg"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		panic(fmt.Sprintf("env file load error with message: %v", err.Error()))
+	}
+	pkg.SetDefaultTeleClient(os.Getenv("APIKEY"))
 	router.SetupHandler()
 	router.Router.Start(":6007")
 }
