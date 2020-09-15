d275393275589d5f136dc855a18113d2e6cf7be8
Entalizen <danielarya185@gmail.com>
2020-09-15 16:11:02 +0700

    Add: function to pair a partner, read partner by user id, and scheduler to matching the user queue

---
+++ internal/service/user.go
@@ -0,0 +1,42 @@
+package service
+
+import (
+	"time"
+
+	"github.com/Satssuki/tele-anon-bot-queue/internal/domain/model"
+	"github.com/Satssuki/tele-anon-bot-queue/internal/module"
+)
+
+var userSessionCache = module.SafeCache{}
+
+// ReadUserPartner ..
+func ReadUserPartner(userID string) (string, bool) {
+	return userSessionCache.Read(userID)
+}
+
+// WritePartner ..
+func WritePartner(userID, partnerID string) {
+	userSessionCache.Insert(userID, partnerID)
+	userSessionCache.Insert(partnerID, userID)
+}
+
+// RemovePartner ..
+func RemovePartner(userID, partnerID string) {
+	userSessionCache.Delete(userID)
+	userSessionCache.Delete(partnerID)
+}
+
+// UserWorkerQueue ..
+func UserWorkerQueue() {
+	go func() {
+		defUserQueue := model.DefaultUserQueue()
+		for {
+			if defUserQueue.Len() > 1 {
+				userID := defUserQueue.Take().(string)
+				partnerID := defUserQueue.Take().(string)
+				WritePartner(userID, partnerID)
+			}
+			time.Sleep(time.Millisecond * 15)
+		}
+	}()
+}
