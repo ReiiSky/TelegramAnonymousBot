fc2a808b3b8d9724150bac9243a3be4e97acf00e
Entalizen <danielarya185@gmail.com>
2020-09-15 12:17:04 +0700

    Add: Wrapping Queue and Cache to provide more functionalities and safety

---
+++ internal/domain/model/user_cache.go
@@ -0,0 +1,67 @@
+package model
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/Satssuki/tele-anon-bot-queue/internal/module"
+)
+
+// UserQueue function to wrap Queue and Cache for more functionalities
+type UserQueue struct {
+	Queue module.SimpleQueue
+	Cache module.SafeCache
+	sync.Mutex
+}
+
+func (userQ *UserQueue) delCache(userID string) {
+	userQ.Cache.Delete(userID)
+}
+
+func (userQ *UserQueue) insertCache(userID string) {
+	userQ.Cache.Insert(userID, "1")
+}
+
+// Len ..
+func (userQ *UserQueue) Len() int {
+	return userQ.Queue.Len()
+}
+
+// Take ..
+func (userQ *UserQueue) Take() interface{} {
+	userQ.Lock()
+	v := userQ.Queue.Take()
+	userQ.delCache(fmt.Sprint(v))
+	userQ.Unlock()
+	return v
+}
+
+// Insert ..
+func (userQ *UserQueue) Insert(v interface{}) {
+	vString := fmt.Sprint(v)
+	if !userQ.IsExist(vString) {
+		userQ.Lock()
+		userQ.Queue.Insert(v)
+		userQ.insertCache(vString)
+		userQ.Unlock()
+	}
+}
+
+// IsExist check is user already appended to user queue
+func (userQ *UserQueue) IsExist(userID string) bool {
+	_, found := userQ.Cache.Read(userID)
+	return found
+}
+
+var userQueue *UserQueue
+
+// DefaultUserQueue ..
+func DefaultUserQueue() *UserQueue {
+	if userQueue == nil {
+		userQueue = &UserQueue{
+			Queue: module.SimpleQueue{},
+			Cache: module.SafeCache{},
+		}
+	}
+	return userQueue
+}
