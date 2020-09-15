2b94f517616eb8ae56ab9c7249a624f2abb4fe8b
Entalizen <danielarya185@gmail.com>
2020-09-15 16:27:37 +0700

    Add: Create function to start a worker to deliver message through bot

---
+++ internal/lib/teledeliver.go
@@ -0,0 +1,45 @@
+package lib
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/Satssuki/tele-anon-bot-queue/internal/service"
+	"github.com/Satssuki/tele-anon-bot-queue/pkg"
+)
+
+// MessageDeliverQueue queue to delivering message through worker
+// i using channel to prevent goroutine ownership race
+var MessageDeliverQueue chan *pkg.Message
+
+// StartWorker ..
+func StartWorker(workerCount uint) {
+	for i := 0; i < workerCount; i++ {
+		workerCount()
+	}
+}
+
+func workerFunction() {
+	c := pkg.GetDefaultTeleClient()
+	username := os.Getenv("BOTUSERNAME")
+	go func() {
+		for {
+			message := <-MessageDeliverQueue
+			partnerID, isExist := service.ReadUserPartner(fmt.Sprint(message.Content.From.ID))
+			if isExist && message.Content.Reply.From.Username != username {
+
+				textBuilder := c.TextMessageBuilder().ChatID(partnerID).Content(message.Content.Text)
+				replyID := message.Content.Reply.MessageID
+
+				if replyID > 0 {
+					textBuilder.ReplyTo(fmt.Sprint(replyID))
+				}
+				_, err := c.Push(textBuilder)
+
+				if err != nil {
+					MessageDeliverQueue <- message
+				}
+			}
+		}
+	}()
+}
