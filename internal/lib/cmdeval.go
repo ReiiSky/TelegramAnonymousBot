f0b75230918ca12fec6b7ffc5782c644aa81ecd8
Entalizen <danielarya185@gmail.com>
2020-09-15 16:26:45 +0700

    Add: forming SearchCommand but not completely yet

---
+++ internal/lib/cmdeval.go
@@ -0,0 +1,45 @@
+package lib
+
+import (
+	"fmt"
+
+	"github.com/Satssuki/tele-anon-bot-queue/internal/domain/model"
+	"github.com/Satssuki/tele-anon-bot-queue/internal/service"
+	"github.com/Satssuki/tele-anon-bot-queue/pkg"
+)
+
+// CommandList i make this variable to reduce usage of if else
+var CommandList = map[string]func(*pkg.Message) (string, error){
+	"/search": SearchCommand,
+	"/stop":   StopCommand,
+	"/next":   NextCommand,
+	"/stats":  StatsCommand,
+}
+
+// SearchCommand ..
+func SearchCommand(msg *pkg.Message) (string, error) {
+	userID := fmt.Sprint(msg.Content.From.ID)
+	partnerID, isExist := service.ReadUserPartner(userID)
+	if !isExist || len(partnerID) < 2 {
+		if model.DefaultUserQueue().IsExist(userID) {
+			return "You already added before to matching queue", nil
+		}
+		return "You added to matching queue", nil
+	}
+	return "You already had a partner", nil
+}
+
+// StopCommand ..
+func StopCommand(msg *pkg.Message) (string, error) {
+	return "You stopped the chatting with current partner", nil
+}
+
+// NextCommand ..
+func NextCommand(msg *pkg.Message) (string, error) {
+	return "You skipped current partner", nil
+}
+
+// StatsCommand ..
+func StatsCommand(msg *pkg.Message) (string, error) {
+	return "heheheheh", nil
+}
